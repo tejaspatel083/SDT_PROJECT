a4b453dbe67cbcab23fe2c72a13754639fd9ea37
tejaspatel083 <49158242+tejaspatel083@users.noreply.github.com>
2020-05-20 21:00:22 -0400

    java code updated

--- app/src/main/java/com/example/myrecipe_sdt/CreateAccount.java
+++ app/src/main/java/com/example/myrecipe_sdt/CreateAccount.java
@@ -1,14 +1,67 @@
 package com.example.myrecipe_sdt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CreateAccount extends AppCompatActivity {
+
+    private EditText user_name,user_email,user_age,user_phone,user_pwd1,user_pwd2;
+    private Button createbtn;
+    //private FirebaseAuth firebaseAuth;
+    private String name,email,phone,age,password,repassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        user_name = findViewById(R.id.CreateName);
+        user_email = findViewById(R.id.CreateEmail);
+        user_age = findViewById(R.id.CreateAge);
+        user_phone = findViewById(R.id.CreatePhone);
+        user_pwd1 = findViewById(R.id.CreatePassword);
+        user_pwd2 = findViewById(R.id.CreateRePassword);
+        createbtn = findViewById(R.id.CreateBtn);
+        //firebaseAuth = FirebaseAuth.getInstance();
+
+
+        createbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                name = user_name.getText().toString().trim();
+                email = user_email.getText().toString().trim();
+                age = user_age.getText().toString().trim();
+                phone = user_phone.getText().toString().trim();
+                password = user_pwd1.getText().toString().trim();
+                repassword = user_pwd2.getText().toString().trim();
+
+
+                if (name.length()==0 || email.length()==0 || age.length()==0 || phone.length()==0 || password.length()==0 || repassword.length()==0)
+                {
+                    Toast.makeText(CreateAccount.this, "Enter All Details", Toast.LENGTH_SHORT).show();
+                }
+                else if (password.equals(repassword))
+                {
+                    Toast.makeText(CreateAccount.this, "Account Created", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(CreateAccount.this, "Password Mismatched", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myrecipe_sdt/MainActivity.java
+++ app/src/main/java/com/example/myrecipe_sdt/MainActivity.java
@@ -2,13 +2,66 @@ package com.example.myrecipe_sdt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginbtn,createbtn;
+    private TextView forgotpwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginbtn = (Button)findViewById(R.id.MainLoginBtn);
+        createbtn = (Button)findViewById(R.id.MainCreateBtn);
+        forgotpwd = (TextView)findViewById(R.id.MainForgotPassword);
+
+        // getSupportActionBar().setTitle(Html.fromHtml("<font color=\"black\">" + getString(R.string.app_name) + "</font>"));
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("MissingPermission")
+            @Override
+            public void onClick(View v) {
+
+                Vibrator vb = (Vibrator)   getSystemService(Context.VIBRATOR_SERVICE);
+                vb.vibrate(20);
+                Intent i = new Intent(MainActivity.this,HomePage.class);
+                startActivity(i);
+            }
+        });
+
+        createbtn.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("MissingPermission")
+            @Override
+            public void onClick(View v) {
+
+                Vibrator vb = (Vibrator)   getSystemService(Context.VIBRATOR_SERVICE);
+                vb.vibrate(20);
+                Intent i = new Intent(MainActivity.this,CreateAccount.class);
+                startActivity(i);
+
+            }
+        });
+
+        forgotpwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Vibrator vb = (Vibrator)   getSystemService(Context.VIBRATOR_SERVICE);
+                vb.vibrate(20);
+                Intent i = new Intent(MainActivity.this,ForgotPassword.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
