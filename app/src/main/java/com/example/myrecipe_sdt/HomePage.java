38c65936e8a7263cbde9b218426465e165e678e9
sahil <sahil33rana@gmail.com>
2020-05-24 18:09:54 -0400

    ListItemSelected Action implemented

--- app/src/main/java/com/example/myrecipe_sdt/HomePage.java
+++ app/src/main/java/com/example/myrecipe_sdt/HomePage.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -40,6 +42,16 @@ public class HomePage extends AppCompatActivity {
         arrayList.add(new RecipeNameList("Sandwich"));
 
         listView.setAdapter(adapter);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent intent = new Intent(HomePage.this,RecipeDescription.class);
+                startActivity(intent);
+            }
+        });
 
 
 
